6cad050ef0d085ea63325ec2aac05a1c31dc4448
Slavik Atamanchuk <70356320+yatamanchuk@users.noreply.github.com>
2020-10-27 15:55:31 -0500

    hw6 commit

---
+++ FishingBuddy/src/main/java/fishing/data/UserRepository.java
@@ -0,0 +1,9 @@
+package fishing.data;
+
+import fishing.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+}
